06041d3b5cb9b97277cea5cd6d9c7cb5c44f2b1b
michalbilinski <neveride@gmail.com>
2023-01-25 10:18:29 +0100

    Added Garden\GetAllByName method

--- backend/MatchYourGarden.Services/GardenService.cs
+++ backend/MatchYourGarden.Services/GardenService.cs
@@ -1,6 +1,7 @@
 ﻿using MatchYourGarden.DataModel;
 using MatchYourGarden.Persistence;
 using MatchYourGarden.Services.Contracts;
+using System;
 
 namespace MatchYourGarden.Services
 {
@@ -9,6 +10,17 @@ namespace MatchYourGarden.Services
         public GardenService(IDataContext dataContext) : base(dataContext)
         {
 
+        }
+
+        public ServiceResponse<Garden[]> GetAllByName(string name)
+        {
+            if (name == null || name.Length < 3)
+            {
+                return new ServiceResponse<Garden[]>("Name should be at least 3 letters long.", 404);
+            }
+
+            var matchingGardens = _dataContext.Gardens.Where(g => g.Name.Contains(name)).ToArray();
+            return new ServiceResponse<Garden[]>(matchingGardens);
         }
     }
 }

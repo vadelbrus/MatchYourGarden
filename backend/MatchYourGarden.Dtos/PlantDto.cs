22e998565f644bf78e4654cf02932015f50bb6ea
michalbilinski <neveride@gmail.com>
2023-03-14 18:47:19 +0100

    Minor fix, so we are able to create plants without images

--- backend/MatchYourGarden.Dtos/PlantDto.cs
+++ backend/MatchYourGarden.Dtos/PlantDto.cs
@@ -4,7 +4,7 @@
     {
         public string LatinName { get; set; }
         public string? Description { get; set; }
-        public ImageDto[] Images { get; set; }
+        public ImageDto[]? Images { get; set; }
         public List<GardenListItemDto>? Gardens { get; set; }
     }
 }
